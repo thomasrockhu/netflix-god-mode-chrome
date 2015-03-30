575368997ac058fe2ee3a6b71b0a285eaf572738
Thomas Hu <tom@redbeacon.com>
2015-03-30 15:55:21 -0700

    Upload code up to unreleased 1.2.4

--- js/god-mode.js
+++ js/god-mode.js
@@ -1,11 +1,4 @@
 var godMode = function() {
-    if (window.location.href.indexOf('netflix.com') === -1) {
-        if (confirm('Please open Netflix and try again. Would you like me to redirect you to netflix.com?')) {
-            window.location.href = 'http://netflix.com'
-        } else {
-            return false;
-        }
-    }
     _.map(_.toArray(document.getElementsByClassName('slider')), function(e) {
       e.parentNode.replaceChild(e.firstChild, e)
     });
